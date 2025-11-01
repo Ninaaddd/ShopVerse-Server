f015dbc0f4f1d1f1045bd608e4dd519d81ff60c0
Ninad <ninadkulkarni3615@gmail.com>
2025-11-01 13:44:39 +0530

    Removed userId from order-routes

--- routes/shop/order-routes.js
+++ routes/shop/order-routes.js
@@ -12,7 +12,7 @@ const router = express.Router();
 
 router.post("/create", authMiddleware,createOrder);
 router.post("/capture", authMiddleware,capturePayment);
-router.get("/list/:userId", authMiddleware, getAllOrdersByUser);
+router.get("/list/", authMiddleware, getAllOrdersByUser);
 router.get("/details/:id", authMiddleware, getOrderDetails);
 
 module.exports = router;
