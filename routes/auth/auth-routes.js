9b0993b6c8209dfdefdd6c01fa30505e20866325
Ninad <ninadkulkarni3615@gmail.com>
2025-11-04 12:42:16 +0530

    error checks

--- routes/auth/auth-routes.js
+++ routes/auth/auth-routes.js
@@ -56,6 +56,7 @@ router.get("/check-auth", async(req, res) => {
       },
     });
   } catch (error) {
+    console.error("error authentication",err.message);
     res.json({
       success: false,
       message: "Invalid or expired token",
