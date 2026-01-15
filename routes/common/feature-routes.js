646dcddf2c38d133d48a2335ae5b377c8c92fee3
Ninad <ninadkulkarni3615@gmail.com>
2026-01-15 23:13:38 +0530

    Added delete functionality for feature image

--- routes/common/feature-routes.js
+++ routes/common/feature-routes.js
@@ -3,11 +3,13 @@ const express = require("express");
 const {
   addFeatureImage,
   getFeatureImages,
+  deleteFeatureImage,
 } = require("../../controllers/common/feature-controller");
-const { authenticate } = require("../../middlewares/auth");
+const { authenticate, requireAdmin } = require("../../middlewares/auth");
 const router = express.Router();
 
-router.post("/add", authenticate, addFeatureImage);
+router.post("/add", authenticate, requireAdmin, addFeatureImage);
 router.get("/get", getFeatureImages);
+router.delete("/delete/:id", authenticate, requireAdmin, deleteFeatureImage);
 
 module.exports = router;
